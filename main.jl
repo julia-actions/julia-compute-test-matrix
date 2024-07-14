4a659b72d36d1b133d67dcb77c8a500799a9660b
David Anthoff <anthoff@berkeley.edu>
2024-07-14 13:55:08 +0200

    Compute correct test matrix

--- main.jl
+++ main.jl
@@ -20,7 +20,7 @@ function construct_channel_list(version)
     end
 end
 
-versions = construct_channel_list.([
+versions = [
     v"1.0.5",
     v"1.1.1",
     v"1.2.0",
@@ -32,19 +32,36 @@ versions = construct_channel_list.([
     v"1.8.5",
     v"1.9.4",
     v"1.10.4"
-])
-
-if Sys.isapple() && ENV["JULIA_ARCH"] == "aarch64"
-    filter!(i -> i.first >= v"1.8.0", versions)
-end
-
-filter!(i -> i[1] in version_spec, versions)
-
-flat_versions = collect(Iterators.flatten(map(i->i.second, versions)))
-
-flat_versions = [
-    Dict("os" => "ubuntu-latest", "juliaup_channel" => "release"),
-    Dict("os" => "macos-latest", "juliaup_channel" => "release"),
 ]
 
-GitHubActions.set_output("test-matrix", flat_versions)
+filter!(i -> i in version_spec, versions)
+
+function add_matrix_entries!(results, v)
+    push!(results, Dict("os" => "windows-latest", "juliaup_channel" => "$v~x64"))
+    push!(results, Dict("os" => "windows-latest", "juliaup_channel" => "$v~x86"))
+    push!(results, Dict("os" => "ubuntu-latest", "juliaup_channel" => "$v~x64"))
+    push!(results, Dict("os" => "ubuntu-latest", "juliaup_channel" => "$v~x86"))
+    if v==v"1.4.2"
+        # For some reason Julia 1.4 doesn't work on macos-13, so we downgrade to macos-12
+        push!(results, Dict("os" => "macos-12", "juliaup_channel" => "$v~x64"))
+    else
+        push!(results, Dict("os" => "macos-13", "juliaup_channel" => "$v~x64"))
+    end
+    if v>=v"1.8.0"
+        push!(results, Dict("os" => "macos-latest", "juliaup_channel" => "$v~aarch64"))
+    end
+end
+
+results = []
+
+for v in versions
+    add_matrix_entries!(results, v)
+end
+
+
+# flat_versions = [
+#     Dict("os" => "ubuntu-latest", "juliaup_channel" => "release"),
+#     Dict("os" => "macos-latest", "juliaup_channel" => "release"),
+# ]
+
+GitHubActions.set_output("test-matrix", results)
